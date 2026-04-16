8e7e01b2fe775b1a9e7d1c6c7acbdc8386432079
Mashashiro kurogami <scarletmaniac2@gmail.com>
2026-04-16 19:36:21 +0545

    day 2 complete

---
+++ day-2/index.js
@@ -0,0 +1,17 @@
+console.log('Day-2');
+
+let student = {
+    name: 'alice',
+    age: 22,
+    isEnrolled: true
+};
+console.log(student);
+
+
+let userName, age, student2, programmingLanguage;
+userName = 'scarlet';
+age = 12; 
+student2 = true;
+programmingLanguage = 'javascript';
+
+console.log(userName, age, student2, programmingLanguage);
