0842507b799c5220e2e364b45dc7f8ac48b712d2
Mashashiro kurogami <scarletmaniac2@gmail.com>
2026-04-18 12:26:29 +0545

    completed day-4 exercises

---
+++ day-4/exercise-4.js
@@ -0,0 +1,69 @@
+console.log("exercise-4");
+//1. output
+console.log("its a normal dayy");
+
+//2. ATM cash withdrawl with 100 multiples only
+
+function withdraw(withdrawnAmount) {
+  if (withdrawnAmount % 100 === 0) {
+    return "withdraw successful";
+  } else {
+    return "invalid amount";
+  }
+}
+console.log(withdraw(500));
+
+//3. Calculator with switch case
+
+function calculator(value1, value2, operator) {
+  switch (operator) {
+    case "+":
+      return value1 + value2;
+      break;
+    case "-":
+      return value1 - value2;
+      break;
+    case "*":
+      return value1 * value2;
+      break;
+    case "/":
+      return value2 === 0 ? 'value cannot be dividev by 0' : value1 / value2;
+      break;
+    case "%":
+      return value1 % value2;
+      break;
+    default:
+      return 'invalid value';
+  }
+}
+console.log(calculator(3, 0, '/'));
+
+//4. movie ticket
+
+function ticket(age) {
+    if (age < 18) {
+        return '$3';
+    } else if (age >= 18 && age<= 60) {
+        return '$10';
+    } else {
+        return '$8';
+    }
+}
+console.log(ticket(70));
+
+//5. triangle type
+function triangle(side1, side2, side3) {
+    //valid triangle checker
+    if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
+        return 'not a valid triangle';
+    }
+    //triangle type checker
+    if (side1 === side2 && side2 === side3) {
+        return 'equilateral triangle';
+    } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+        return 'isosceles triangle';
+    } else {
+        return 'scalene triangle';
+    }
+}
+console.log(triangle(3, 5, 3));
---
+++ day-4/index-4.js
@@ -0,0 +1,63 @@
+console.log("day-4");
+
+//switch case
+
+let pos = 4;
+switch (pos) {
+  case 1:
+    console.log("print1");
+    break;
+  case 2:
+    console.log("print12");
+    break;
+  case 3:
+    console.log("print13");
+    break;
+  case 4:
+    console.log("print14");
+    break;
+  case 5:
+    console.log("print15");
+    break;
+  default:
+    console.log("nothing is matched");
+}
+
+let day = 6;
+switch (day) {
+  case 1:
+    console.log("sunday");
+    break;
+  case 2:
+    console.log("monday");
+    break;
+  case 3:
+    console.log("tuesday");
+    break;
+  case 4:
+    console.log("wednesday");
+    break;
+  case 5:
+    console.log("thursday");
+    break;
+  case 6:
+    console.log("friday");
+    break;
+  case 7:
+    console.log("saturday");
+    break;
+  default:
+    console.log("invalid day");
+}
+
+let name = "google";
+switch (name) {
+  case "tapaScript":
+    console.log("40 days js");
+    break;
+  case "google":
+    console.log("searches");
+    break;
+  default:
+    console.log("you are neither");
+}
